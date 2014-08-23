9c0f04ff0c27f3c3b643b9bde42ec8835350439c
kelvindzd <zidong.du.2013@smu.edu.sg>
2014-08-23 12:37:16 +0800

    Update app.js

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,22 +1,2 @@
-var app = angular.module("sampleApp", ["firebase"]);
 
-// let's create a re-usable factory that generates the $firebaseSimpleLogin instance
-app.factory("simpleLogin", ["$firebaseSimpleLogin", function($firebaseSimpleLogin) {
-  var ref = new Firebase("https://loginandrules.firebaseio.com/");
-  return $firebaseSimpleLogin(ref);
-}]);
-
-// and use it in our controller
-app.controller("loginCtrl", ["$scope", "simpleLogin", function($scope, simpleLogin) {
-  $scope.auth = simpleLogin;
-}])
-
-app.controller("msgCtrl", function($scope, $firebase) {
-  var ref = new Firebase("https://loginandrules.firebaseio.com/");
-  var sync = $firebase(ref);
-  $scope.messages = sync.$asArray();
-  $scope.addMessage = function(text) {
-    $scope.messages.$add({text: text});
-  }
-});
 
